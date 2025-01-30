5379aa8c31a7ebff271d9f113813760429dbc892
MasterGun2006 <maxymmaster06@gmail.com>
2025-01-30 20:02:39 +0200

    ns_andre.py добавлено, модіфікований оригінальний код Андре, + мною додано ввід типу автомата та Y

--- ns.py
+++ ns.py
@@ -2,24 +2,22 @@
 def graph_func(peak_name, amount_of_peaks):
     state_coding = {}
     order_of_codes = []
+    indecies = {}
     print("Введіть кодування вершин")
     for i in range(amount_of_peaks):
         print(f"Кодування вершини {peak_name}[{i}]:")
         state_coding[peak_name+str(i)] = input()
+    print(state_coding)
     print("Запишіть як поєднанні вершини. Формат 000>001>010>011")
     temp  = input().replace(">", "  >  ")
     order_of_codes = temp.split(" ")
     print(f"Порядок кодування вершин{order_of_codes}")
     temp = temp.replace(">", "")
     temp = temp.replace(" ", "")
-    order_of_codes = temp.split(" ")
-    print(f"Порядок кодування вершин{order_of_codes}")
-    indecies = []
-    for i in amount_of_peaks:
-        temp = state_coding.get(peak_name + str(i))
-         for j in range(order_of_codes):
-             if temp == order_of_codes:
-                 indecies.append(i)
+    for i in range(enumerate(order_of_codes)):
+        indecies[order_of_codes].update(state_coding[order_of_codes[i]])
+    
+    
     print(f"indecies: {indecies}")
 
 
