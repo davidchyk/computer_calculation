9d2c10ad69c429caaf1fa02772124b64ec5db202
MasterGun2006 <maxymmaster06@gmail.com>
2025-01-31 20:27:06 +0200

    ddddddddddxdd

--- ns_andre.py
+++ ns_andre.py
@@ -104,7 +104,7 @@ def graph_func():
                 break
 
     print(values_of_Y_coding)
-    print(triggers(type_of_trigger, transition_list))
+    triggers(type_of_trigger, transition_list)
     # Створення словника переходів
     for i in range(len(transition_list) - 1):
         current_state = transition_list[i]
@@ -139,26 +139,34 @@ def triggers(type_of_trigger, transition_list):
         if type(loop_range) is float:
             loop_range += 1
             loop_range = int(loop_range)
-        
+        print("is working")
         for i in range(loop_range):
             while True:
+                print(f"org:{transition_list}")
                 temp = transition_list[i]
-                for i in range(len(temp)):
-                    if temp[i] == 0 and transition_list[i+1] == 0:
+                temp_next = transition_list[i+1]
+                print(f"len temp: {len(temp)}")
+                print(f"len temp_next: {len(temp_next)}")
+                print(f"range len temp{range(len(temp))}")
+                for j in range(len(temp)-1):
+                    print(f"j: {j}")
+                    print(f"cycle: {temp[j]}, second: {str(transition_list[i+1])[j]}")
+                    if temp[j] == "0" and temp_next[j+1] == "0":
                         coding_trigger[f"R{i+1}"] = "*"
                         coding_trigger[f"S{i+1}"] = "0"
-                    elif temp[i] == 0 and transition_list[i+1] == 1:
+                    elif temp[j] == "0" and temp_next[j+1] == "1":
                         coding_trigger[f"R{i+1}"] = "0"
                         coding_trigger[f"S{i+1}"] = "1"
-                    elif temp[i] == 1 and transition_list[i+1] == 0:
+                    elif temp[j] == "1" and temp_next[j+1] == "0":
                         coding_trigger[f"R{i+1}"] = "1"
                         coding_trigger[f"S{i+1}"] = "0"
-                    elif temp[i] == 1 and transition_list[i+1] == 1:
+                    elif temp[j] == "1" and temp_next[j+1] == "1":
                         coding_trigger[f"R{i+1}"] = "0"
                         coding_trigger[f"S{i+1}"] = "*"
                 
                 break
-    return coding_trigger
+         
+    print(coding_trigger)
 
 if __name__ == '__main__':
     graph_func()
