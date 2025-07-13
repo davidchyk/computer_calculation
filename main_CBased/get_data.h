7551a2323bce0d96002748bde257f2fa06bc6c40
Artem Dyachenko <marshal.dyachenko@gmail.com>
2025-07-14 00:13:18 +0300

    deleted C files

--- main_CBased/get_data.h
+++
@@ -1,9 +0,0 @@
-#include <stddef.h>
-
-// Функція для перевірки, чи є рядок валідним числом
-int isFloat(const char *str);
-
-// Функція для перевірки рядка та створення масиву цілих чисел
-int *parseDynamicStringToArray(const char *data, int optional, size_t *arraySize);
-
-char **splitBASIS(char *dynamicString);
