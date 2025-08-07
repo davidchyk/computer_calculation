e4910b4968884ac6e363059f8f6f3914f8bf837b
Artem <marshal.dyachenko@gmail.com>
2025-08-07 05:47:23 +0300

    tested minimization functions: todo -> completed veich -> completed the main program -> todo arithmetic -> integrate auto -> done

--- VEICH_module/BIGtest.py
+++ VEICH_module/BIGtest.py
@@ -1,7 +1,7 @@
 # BITTEST.py
 
 from minimization import minimize_dnf_as_implicants, minimize_cnf_as_implicants
-from minimizatonTEST import quine_mccluskey_dnf, quine_mccluskey_cnf
+from normalmini import minimized_implicants
 
 def test_compare(num_vars: int, values: list[int], is_dnf: bool):
     """
@@ -15,11 +15,11 @@ def test_compare(num_vars: int, values: list[int], is_dnf: bool):
 
     if is_dnf:
         implicants_sympy = minimize_dnf_as_implicants(num_vars, values)[1]
-        result_qmc = quine_mccluskey_dnf(num_vars, values)[1]
+        result_qmc = minimized_implicants(num_vars, values, is_dnf=True)
         form = "МДНФ"
     else:
         implicants_sympy = minimize_cnf_as_implicants(num_vars, [x for x in range(number_of_sets) if x not in values])[1]
-        result_qmc = quine_mccluskey_cnf(num_vars, [x for x in range(number_of_sets) if x not in values])[1]
+        result_qmc = minimized_implicants(num_vars, [x for x in range(number_of_sets) if x not in values], is_dnf=False)
         form = "МКНФ"
 
     print(f"--- Порівняння результатів ({form}) для {num_vars} аргументів ---")
@@ -34,9 +34,11 @@ if __name__ == '__main__':
     test_compare(num_vars=3, values=[0, 1, 2, 5, 6], is_dnf=True)
     test_compare(num_vars=4, values=[0, 3, 5, 6, 9, 10, 12, 15], is_dnf=True)
     test_compare(num_vars=4, values=[1, 4, 6, 7, 8, 10, 11, 13, 15], is_dnf=True)
+    test_compare(num_vars=9, values=[1, 2, 3], is_dnf=True)
 
     # === CNF приклади ===
     test_compare(num_vars=3, values=[0, 2, 4], is_dnf=False)
     test_compare(num_vars=3, values=[1, 2, 3, 6, 7], is_dnf=False)
     test_compare(num_vars=4, values=[2, 3, 5, 7, 11, 13], is_dnf=False)
     test_compare(num_vars=4, values=[0, 1, 3, 4, 6, 8, 9, 12, 14], is_dnf=False)
+    test_compare(num_vars=9, values=[1, 2, 3], is_dnf=False)
