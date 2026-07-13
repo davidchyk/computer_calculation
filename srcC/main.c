04b2d416fef98e37ca38ea3ad2ee9d0f45ee5a25
Artem Davydchuk <marshal.dyachenko@gmail.com>
2026-07-13 21:32:25 +0300

    updt. for previous updates

--- srcC/main.c
+++ srcC/main.c
@@ -7,30 +7,30 @@
 #include <limits.h>
 #include <errno.h>
 
-#include "gui/gui.h"
+#include "utils/typing.h"
+#include "utils/string_process.h"
+#include "utils/utils.h"
 
-#include "string_process/string_process.h"
+#include "gui/gui.h"
 
 #include "core/normal_form.h"
 #include "core/operator_form.h"
 
-#include "typing.h"
-
 int main(void) {
 
-    printf("Computer Calculation Tool\nAuthor: Davydchuk Artem\n\n");
+    printf("Computer Calculation Helper\nAuthor: Davydchuk Artem\n\n");
 
     function_t test = {
 
         .index = 0,
         .argsNum = 3,
-        .setsNum = 7,
-        .setsArray = (int[]){0, 1, 2, 3, 4, 5, 6},
+        .setsNum = 3,
+        .setsArray = (int[]){0, 1, 2},
         .argsArray = (char*[]){"{x_3}", "{x_2}", "{x_1}"},
 
         .from_expression_created = false,
 
-        .basis = AND_OR,
+        .basis = NAND_AND,
         .first_basisNumber = 2,
         .second_basisNumber = 2,
 
@@ -49,7 +49,7 @@ int main(void) {
     set_normal_form(&test);
     set_operator_form(&test);
 
-    printf("normal_form: \n%s\n", test.normal_form.data);
+    // printf("normal_form: \n%s\n", test.normal_form.data);
     printf("operator_form: \n%s\n", test.operator_form.data);
 
     test.normal_form = (string){NULL, 0u, 0u};
